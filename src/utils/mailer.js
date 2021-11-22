d4d64e88a28dd421070dc6e515e2bedea5466ea0
ubani-machine <liegeubani@gmail.com>
2021-11-22 11:59:14 +0100

    done

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,36 +1,36 @@
 var nodemailer = require("nodemailer");
+const Email = process.env.EMAIL;
+const passord = process.env.EMAILPASS;
 
-const sendVerificationEmail = (email, token ) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "liegeubani@gmail.com",
-            pass: "nwaubani111",
-        },
-    });
+const sendVerificationEmail = (email, token) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: Email,
+      pass: passord,
+    },
+  });
 
-    const mailOptions = {
-      from: "liegeubani@gmail.com",
-      to: email,
-      subject: "Verify your new account",
-      html: `<p>To verify your email address, please click the link below </p> <br>
+  const mailOptions = {
+    from: "liegeubani@gmail.com",
+    to: email,
+    subject: "Verify your new account",
+    html: `<p>To verify your email address, please click the link below </p> <br>
         
         <h4>
         <a href="http://localhost:8000/?verification=${token}"target="_blank"> Verify Your Account </a>
         </h4>
         `,
-    };
+  };
 
-
-     transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log('email error', error.message);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
-    
-}
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("email error", error.message);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
 
 
 module.exports = sendVerificationEmail;
