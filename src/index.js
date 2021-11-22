f03456000ba377e8d784f57e3103ea117280a10c
ubani-machine <liegeubani@gmail.com>
2021-11-22 10:10:39 +0100

    .....

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const route  = require("./router")
 
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000;
 
 // handle middleware to convert request to json format
 app.use(express.json());
